ae373976b431a0ff1b57bdc03eb977f6c98e7882
Павел <pirogov-p@list.ru>
2023-03-07 14:11:10 +0300

    изменяемые типы: list, dict, set

--- 1.2 movie.py
+++ 1.2 movie.py
@@ -16,3 +16,43 @@ q = p
 q.append(3)
 print (q)
 print (p)
+
+x = [1, 2, 3]
+y = x
+y.append(4)
+
+s = "123"
+t = s
+t = t + "4"
+print(s)
+print(t)
+print(str(x) + " " + s)
+
+s = 123
+t = s
+t *= 54
+print(s)
+print(t)
+
+
+x=[1,2,3]
+y=x
+y=y*2
+print(x)
+#[1, 2, 3]
+print(y)
+#[1, 2, 3, 1, 2, 3]
+
+
+x=[1,2,3]
+y=x
+y*=2
+print(x)
+#[1, 2, 3, 1, 2, 3]
+print(y)
+#[1, 2, 3, 1, 2, 3]
+
+x=[1,2,3]
+type(x)
+type(4)
+type(type(x))
