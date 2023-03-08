aa9a24eb67cc66a20aa88acc5fc7a01c15c6314b
Павел <pirogov-p@list.ru>
2023-03-09 00:42:39 +0300

    решение задачи через словарь и рекурсию

--- 1.4.10.py
+++ 1.4.10.py
@@ -1,9 +1,40 @@
 n = int(input())
-command []
-namespaces={
-    'global': {'parent': None, 'vars': []},
-}
+ins = []
+command=[]
+finish = []
+namespaces={"global": ["None", []]}
+def find(space,var):
+    b = namespaces[space]
+    if var in b[1]:
+        finish.append(space)
+    elif space == "global" :
+        finish.append('None')
+        return 0
+    else :
+        c = namespaces[space]
+        find(c[0], var)
+        return 0
+
 for i in range(n):
+    ins =[]
     cmd, namesp, arg = input().split()
+    ins.append(cmd)
+    ins.append(namesp)
+    ins.append(arg)
+    command.append(ins)
+    if cmd == 'add':
+        b = namespaces[namesp]
+        b[1].append(arg)
+        namespaces[namesp] = b
+        #print(namespaces)
+    elif cmd == 'create':
+        namespaces[namesp] = [arg,[]]
+        #print(namespaces)
+    elif cmd == 'get':
+        find(namesp,arg)
+        #print(namespaces)
+#print (namespaces)
 
-
+#print(command)
+for a in finish:
+    print(a)
