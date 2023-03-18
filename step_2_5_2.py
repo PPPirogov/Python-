5025c2c3f68e8caf2e720dcf6f0367e8e766c5ff
Павел <pirogov-p@list.ru>
2023-03-18 17:40:11 +0300

    применяем функцию в функции

---
+++ step_2_5_2.py
@@ -0,0 +1,22 @@
+#x = input().split()
+#print(x)
+#map_obj = map(int,x) #2agrument: function and [a,b,c...] -> f(a),f(b),f(c)...
+#n,k = (int(i) for i in x)
+
+# print (map_obj)
+# n=next(map_obj)
+# k=next(map_obj)
+#print(n+k)
+
+#----------------------
+
+x = input().split()
+xs = (int(i) for i in x)
+
+def even(x):
+    return x%2 ==0
+# evens = filter(even,xs)
+# for i in evens:
+#     print(i)
+events = list(filter(even,xs))
+print(events)
