cac61e1e056373f27d59fdcb181bc95fb08a477a
Павел <pirogov-p@list.ru>
2023-03-20 14:25:56 +0300

    стандартные методы и функции для строк решение задачь: 1 и 2

--- step_3_2_9.py
+++ step_3_2_9.py
@@ -1,10 +1,7 @@
 import sys, re
-pattern = r'\bcat\b'
+pattern = r'z...z'
 for line in sys.stdin:
     line = line.rstrip()
     all_inclusions = re.search(pattern, line)
     if all_inclusions:
         print(line)
-    #print(count(all_inclusions))
-    #if len(all_inclusions) >= 2:
-        #print(line)
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import sys, re
-pattern = r'(cat)'
+pattern = r'z...z'
 for line in sys.stdin:
     line = line.rstrip()
-    all_inclusions = re. search(r"cat\b", line)
-    print(all_inclusions)
+    all_inclusions = re.search(pattern, line)
+    #print(all_inclusions)
+    if all_inclusions:
+        print(line)
     #print(count(all_inclusions))
     #if len(all_inclusions) >= 2:
         #print(line)
