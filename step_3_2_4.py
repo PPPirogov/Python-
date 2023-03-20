d0acd6825146c2976824bad0465982b07ff49086
pppirogov21 <pppirogov21@gmail.com>
2023-03-20 12:20:14 +0800

    регулярные выражение Теория группировка

--- step_3_2_4.py
+++ step_3_2_4.py
@@ -8,3 +8,13 @@ pattern = r'ab?a' # 0 или 1 вхождение
 string = 'aa, aba, abba, abbba, abbba'
 all_inclusions = re.findall(pattern,string)
 print(all_inclusions)
+print(' ')
+
+#pattern1 = r'a[ab]+a'
+pattern1 = r'a[ab]+?a'
+string1 = 'abaaba'
+the_most_long = re.match(pattern1,string1)
+print(the_most_long)
+print(re.match(pattern1,string1))
+print(re.findall(pattern1,string1))
+
---
+++ step_3_2_5.py
@@ -0,0 +1,39 @@
+import re
+
+#pattern = r'text'
+pattern = r'((abc)|(test|text)*)'
+string = 'textexttest'
+match = re.match(pattern,string)
+print(match)
+print(match.groups())
+print('')
+########################################
+
+pattern = r'Hello (abc|test)'
+string = 'Hello abc'
+match = re.match(pattern,string)
+print(match)
+print(match.group(0))
+print(match.group(1))
+
+print('')
+########################################
+
+pattern = r'(\w+)-\1'
+#string = 'test-text'
+string = 'test-test chow-chow'
+#match = re.match(pattern,string)
+#print(match)
+duplicates = re.sub(pattern, r'\1', string)
+print (duplicates)
+
+print('')
+########################################
+
+pattern = r'((\w+)-\2)'
+#string = 'test-text'
+string = 'test-test chow-chow'
+#match = re.match(pattern,string)
+#print(match)
+duplicates = re.findall(pattern, string)
+print (duplicates)
