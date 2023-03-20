7cd8bc379ef63a6262898928ec67af3e34ddf131
Павел <pirogov-p@list.ru>
2023-03-20 11:43:16 +0300

    стандартные методы и функции для строк решение задачи

--- step_3_2_3.py
+++ step_3_2_3.py
@@ -2,10 +2,10 @@ import re
 
 pattern = r' english?'
 string = 'Do you speak english?'
-match = re.search(pattern,string)
+match = re.search(pattern, string)
 
 print(match)
 
 pattern = r' english\?'
-match = re.search(pattern,string)
+match = re.search(pattern, string)
 print(match)
